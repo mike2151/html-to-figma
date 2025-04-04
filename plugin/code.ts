fc37f97c02eee9548b5afee560efe82ea31ea2d8
Mike Abelar <mikeabelar1999@gmail.com>
2025-04-04 14:59:37 -0400

    Get onclick binding working

--- plugin/code.ts
+++ plugin/code.ts
@@ -6,33 +6,36 @@
 // Runs this code if the plugin is run in Figma
 if (figma.editorType === 'figma') {
   // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
+  figma.showUI(__html__, { width: 600, height: 500 });
 
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      // This plugin creates rectangles on the screen.
-      const numberOfRectangles = msg.count;
-
-
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfRectangles; i++) {
-        const rect = figma.createRectangle();
-        rect.x = i * 150;
-        rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-        figma.currentPage.appendChild(rect);
-        nodes.push(rect);
+  figma.ui.onmessage = async (message) => {
+    if (message.type === 'convert') {
+      try {
+        // Parse the HTML and CSS
+        // const htmlContent = message.html;
+        // const cssContent = message.css;
+        
+        // // Create Figma nodes from the HTML and CSS
+        // const nodes = await convertHtmlCssToFigma(htmlContent, cssContent);
+        
+        // // Select the created nodes in Figma
+        // figma.currentPage.selection = nodes;
+        
+        // // Focus the view on the created nodes
+        // figma.viewport.scrollAndZoomIntoView(nodes);
+        
+        // Send success message back to UI
+        figma.ui.postMessage({
+          type: 'success',
+          message: 'Conversion successful! Created '
+        });
+      } catch (error) {
+        // Send error message back to UI
+        figma.ui.postMessage({
+          type: 'error',
+          message: 'Error: ' + (error instanceof Error ? error.message : String(error))
+        });
       }
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
     }
-
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
   };
 }
