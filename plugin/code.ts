ec1cd0cf22ddedda1377fe9b101a5091e3062a0d
Mike Abelar <mikeabelar1999@gmail.com>
2025-04-05 22:17:15 -0400

    Add webpack

--- plugin/code.ts
+++ plugin/code.ts
@@ -1,17 +1,12 @@
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+import { ALLOWED_FIGMA_EDITOR_TYPES } from './constants';
 
-// Runs this code if the plugin is run in Figma
-if (figma.editorType === 'figma') {
+if (ALLOWED_FIGMA_EDITOR_TYPES.includes(figma.editorType)) {
   // This shows the HTML page in "ui.html".
   figma.showUI(__html__, { width: 575, height: 400 });
 
   figma.ui.onmessage = async (message) => {
     if (message.type === 'convert') {
       try {
-        // Parse the HTML and CSS
         const htmlContent = message.html;
         const cssContent = message.css;
 
---
+++ plugin/constants.js
@@ -0,0 +1,1 @@
+export const ALLOWED_FIGMA_EDITOR_TYPES = ['figma'];
