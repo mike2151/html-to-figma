e404eaf9c1c24afd60fac25e220eb42b661c824b
Mike Abelar <mikeabelar1999@gmail.com>
2025-04-15 20:19:24 -0700

    cleanup tailwind code

--- web-app/src/components/Header.tsx
+++ web-app/src/components/Header.tsx
@@ -6,10 +6,7 @@ const Header = () => {
       <div className="container">
         <div className="flex justify-between items-center">
           <div className="flex items-center space-x-4">
-            <div className="w-10 h-10 bg-blue-600 rounded-lg flex items-center justify-center">
-              <span className="text-white font-bold">H2F</span>
-            </div>
-            <span className="text-xl font-bold text-red-500">HTML to Figma</span>
+            <span className="text-xl font-bold text-gray-900 mx-2">HTML to Figma</span>
           </div>
           <nav>
             <ul className="flex space-x-8">
